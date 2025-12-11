ad67e9d0cb05adf3cc25bd2868ea4490f8d8c756
keegan-cmyk <keegan@saddlebrookelife.com>
2025-12-11 13:50:12 -0400

    Rebuild homepage layout with all new sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,29 @@
 import Hero from "./components/Hero";
 import DemoSection from "./components/DemoSection";
 import BenefitsSection from "./components/BenefitsSection";
+import DifferentiatorSection from "./components/DifferentiatorSection";
+import HowItWorksSection from "./components/HowItWorksSection";
+import ComicSection from "./components/ComicSection";
 import WorkflowsSection from "./components/WorkflowsSection";
+import AboutFounder from "./components/AboutFounder";
+import MissionSection from "./components/MissionSection";
 import ContactSection from "./components/ContactSection";
+import SocialBar from "./components/SocialBar";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center">
+    <>
       <Hero />
+      <SocialBar />
       <DemoSection />
       <BenefitsSection />
+      <DifferentiatorSection />
+      <HowItWorksSection />
+      <ComicSection />
       <WorkflowsSection />
+      <AboutFounder />
+      <MissionSection />
       <ContactSection />
-    </main>
+    </>
   );
 }
