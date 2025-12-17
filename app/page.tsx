0869ca7728d493bf8293a96ff891660931e4dcdf
keegan-cmyk <keegan@saddlebrookelife.com>
2025-12-17 15:21:42 -0400

    Add MissionSection to homepage render

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "./components/Hero";
 import SocialBar from "./components/SocialBar";
 import BenefitsSection from "./components/BenefitsSection";
+import MissionSection from "./components/MissionSection";
 import HowItWorksSection from "./components/HowItWorksSection";
 import WorkflowsSection from "./components/WorkflowsSection";
 import AboutFounder from "./components/AboutFounder";
@@ -13,6 +14,7 @@ export default function Home() {
       <Hero />
       <QuickNav />
       <SocialBar />
+      <MissionSection />
       <BenefitsSection />
       <HowItWorksSection />
       <WorkflowsSection />
